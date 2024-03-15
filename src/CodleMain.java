402c8b6d2ce9c7a40b33c15abe6a76dc58e31f76
Janiaktatwit <144467297+Janiaktatwit@users.noreply.github.com>
2024-03-15 09:41:59 -0400

    Created main and other class

---
+++ src/CodleMain.java
@@ -0,0 +1,9 @@
+
+public class CodleMain {
+
+	public static void main(String[] args) {
+		RandomWord.getWord();
+
+	}
+
+}
---
+++ src/RandomWord.java
@@ -0,0 +1,6 @@
+
+public class RandomWord {
+	public static String getWord() {
+		return "w";
+	}
+}
