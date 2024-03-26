b43f56dd22a836ecfbcf397389ad75433e0a20ae
augus <augus@DESKTOP-H11BMHR>
2024-03-26 11:58:34 -0400

    button shenanigans

--- src/codle/v1/CodleMain.java
+++ src/codle/v1/CodleMain.java
@@ -10,6 +10,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
@@ -35,20 +36,32 @@ public class CodleMain extends Application {
 		grid.setPadding(new Insets(25, 25, 25, 25));
 		final Text word = new Text("");
 		final Text word2 = new Text("");
+		TextField entry = new TextField();
+		final Button submit = new Button("Submit");
 		grid.add(word, 1, 1);
 		grid.add(btn, 1, 2);
 		grid.add(word2, 2, 1);
-		grid.add(key, 2, 2);
+		grid.add(key, 1, 3);
+		grid.add(entry, 3, 2);
+		grid.add(submit, 3, 3);
 		RandomWord ranWord = new RandomWord();	
 		grid.setOnKeyPressed(e -> {
 			word2.setText(String.format("%s", e.getCode()));
 		});
+		String g = "coding";
+		//Guess g = new Guess(new RandomWord().toString());
 		
 		btn.setOnMouseClicked(e -> {
 			word.setText(ranWord.randomWord());
 		});
+		submit.setOnMouseClicked(e -> {
+			if (entry.getText().equals(g)) {
+				key.setText("Correct");
+			}
+		});
 		primaryStage.setScene(new Scene(grid, 300, 250));
 		primaryStage.show();
+		
 		
 	} 
 
